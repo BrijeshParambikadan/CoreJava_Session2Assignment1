911f146d1f000260f0b84d4ce40ef5d83b128f88
BrijeshParambikadan <bbavittom@gmail.com>
2016-10-12 21:49:48 +0530

    submitted JavaS2A1 assignment

---
+++ Java_S2A1/src/J_S2A1.java
@@ -0,0 +1,13 @@
+// program to print my name without using semicolon in the program
+
+public class J_S2A1 {
+
+	public static void main(String[] args) {
+					
+        if(System.out.printf("Brijesh Parambikadan") == null) {}
+        
+	}
+
+}
+
+//Thank You
